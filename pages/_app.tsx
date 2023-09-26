7f582851e3d7fe59cbd56380638b9e4e83e9e7cb
sudharsangs <g.s.sudharsan99@gmail.com>
2023-09-26 20:30:13 +0530

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -11,6 +12,7 @@ const inter = Inter({
 function MyApp({ Component, pageProps }: AppProps) {
   return <div className={`${inter.variable} bg-gray-800 text-gray-100`}>
     <Component {...pageProps} />
+    <Analytics />
   </div>
 }
 
