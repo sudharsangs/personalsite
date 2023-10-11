35505bbb91a64bebbf224c1f337fd9df1cc80959
sudharsangs <g.s.sudharsan99@gmail.com>
2023-10-11 13:44:47 +0530

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -10,10 +11,21 @@ const inter = Inter({
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div className={`${inter.variable} bg-gray-800 text-gray-100`}>
-    <Component {...pageProps} />
-    <Analytics />
-  </div>
+  return <>
+    <Script strategy='lazyOnload' src="https://www.googletagmanager.com/gtag/js?id=G-SLSPM9Q29G" />
+    <Script strategy='lazyOnload' id="analytics">
+      {`  window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-SLSPM9Q29G');
+      `}
+    </Script>
+    <div className={`${inter.variable} bg-gray-800 text-gray-100`}>
+      <Component {...pageProps} />
+      <Analytics />
+    </div>
+  </>
 }
 
 export default MyApp
